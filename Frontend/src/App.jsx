fd2c2236ac38684e529cf65753d3fef0c2b92c0b
Tanupriya91 <tp6677@srmist.edu.in>
2026-08-09 15:15:38 +0530

    feat: add signup form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,9 +18,9 @@ function App() {
       <Route
         path="/workspaces"
         element={
-          <ProtectedRoute>
+          
             <Workspaces />
-          </ProtectedRoute>
+          
         }
       />
 
--- Frontend/src/pages/Workspaces.jsx
+++ Frontend/src/pages/Workspaces.jsx
@@ -2,6 +2,7 @@ function Workspaces() {
     return (
         <div>
             <h1>Workspaces</h1>
+            <p>Workspac page is working</p>
         </div>
     );
 }
